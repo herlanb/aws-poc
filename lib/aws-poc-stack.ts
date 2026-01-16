d29b02db4ff5f0b4750f6a8a7d5a46ebb79a2043
Herlan <barin_2@hotmail.com>
2026-01-16 15:04:12 -0300

    add ecs

--- lib/aws-poc-stack.ts
+++ lib/aws-poc-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 export class AwsPocStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,50 +23,101 @@ export class AwsPocStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Para POC - borra todo al hacer destroy
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
-
-    new dynamodb.Table(this, 'PersonasTable-Aux', {
-      tableName: 'Personas-Aux',
-      partitionKey: {
-        name: 'id',
-        type: dynamodb.AttributeType.STRING
-      },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Para POC - borra todo al hacer destroy
-    });
-
 
     // Bucket S3
     const bucket = new s3.Bucket(this, 'CsvBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true, // Para POC - borra todo al hacer destroy
+      autoDeleteObjects: true,
     });
 
-    // Lambda Function
-    const procesarCsvLambda = new lambda.Function(this, 'ProcesarCsvLambda', {
-      functionName: 'ProcesarCsvPersonas',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'procesarCsv.handler',
-      code: lambda.Code.fromAsset('lambda'),
-      environment: {
-        TABLE_NAME: table.tableName,
-      },
-      timeout: cdk.Duration.seconds(30),
+    // ========== SNS + SQS ==========
+    
+    // SNS Topic para notificaciones de S3
+    const topic = new sns.Topic(this, 'CsvUploadTopic', {
+      displayName: 'CSV Upload Notifications',
     });
 
-    // Permisos
-    bucket.grantRead(procesarCsvLambda);
-    table.grantWriteData(procesarCsvLambda);
+    // SQS Queue para procesar mensajes
+    const queue = new sqs.Queue(this, 'CsvProcessQueue', {
+      queueName: 'csv-process-queue',
+      visibilityTimeout: cdk.Duration.seconds(60),
+      retentionPeriod: cdk.Duration.days(1),
+    });
 
-    // Trigger: cuando se sube un .csv
+    // Conectar SNS a SQS
+    topic.addSubscription(new subscriptions.SqsSubscription(queue));
+
+    // S3 notifica a SNS cuando se sube un CSV
     bucket.addEventNotification(
       s3.EventType.OBJECT_CREATED,
-      new s3n.LambdaDestination(procesarCsvLambda),
+      new s3n.SnsDestination(topic),
       { suffix: '.csv' }
     );
 
-    // Outputs
+    // ========== VPC ==========
+    
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2,
+      natGateways: 1, // Para POC, 1 es suficiente
+    });
+
+    // ========== ECS ==========
+    
+    // Cluster ECS
+    const cluster = new ecs.Cluster(this, 'Cluster', {
+      clusterName: 'csv-processor-cluster',
+      vpc,
+      containerInsights: true, // Habilita métricas detalladas
+    });
+
+    // ECR Repository
+    const repository = new ecr.Repository(this, 'Repository', {
+      repositoryName: 'csv-processor',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      emptyOnDelete: true,
+    });
+
+    // Task Definition (Fargate)
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+      family: 'csv-processor-task',
+    });
+
+    // Container
+    const container = taskDefinition.addContainer('Worker', {
+      image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
+      environment: {
+        QUEUE_URL: queue.queueUrl,
+        TABLE_NAME: table.tableName,
+        AWS_REGION: this.region,
+      },
+      logging: ecs.LogDrivers.awsLogs({
+        streamPrefix: 'csv-processor',
+        logRetention: logs.RetentionDays.ONE_WEEK,
+      }),
+    });
+
+    // ECS Service (Fargate)
+    const service = new ecs.FargateService(this, 'Service', {
+      cluster,
+      taskDefinition,
+      serviceName: 'csv-processor-service',
+      desiredCount: 2, // 2 workers
+      assignPublicIp: false, // En subnets privadas
+      enableExecuteCommand: true, // Para debugging
+    });
+
+    // ========== PERMISOS ==========
+    
+    bucket.grantRead(taskDefinition.taskRole);
+    table.grantWriteData(taskDefinition.taskRole);
+    queue.grantConsumeMessages(taskDefinition.taskRole);
+
+    // ========== OUTPUTS ==========
+    
     new cdk.CfnOutput(this, 'BucketName', {
       value: bucket.bucketName,
       description: 'Sube tu CSV aquí',
@@ -69,6 +126,31 @@ export class AwsPocStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'TableName', {
       value: table.tableName,
       description: 'Tabla DynamoDB',
+    });
+
+    new cdk.CfnOutput(this, 'QueueUrl', {
+      value: queue.queueUrl,
+      description: 'SQS Queue URL',
+    });
+
+    new cdk.CfnOutput(this, 'ClusterName', {
+      value: cluster.clusterName,
+      description: 'ECS Cluster Name',
+    });
+
+    new cdk.CfnOutput(this, 'ServiceName', {
+      value: service.serviceName,
+      description: 'ECS Service Name',
+    });
+
+    new cdk.CfnOutput(this, 'RepositoryUri', {
+      value: repository.repositoryUri,
+      description: 'ECR Repository URI',
+    });
+
+    new cdk.CfnOutput(this, 'LogGroup', {
+      value: `/ecs/csv-processor`,
+      description: 'CloudWatch Log Group',
     });
   }
 }
